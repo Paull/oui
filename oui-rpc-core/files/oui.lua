555cb1d2581b0d1f6d4b566f7e75538f2344a98f
Paull <paull@qq.com>
2020-02-17 18:56:48 +0800

    reduce more

--- oui-rpc-core/files/oui.lua
+++ oui-rpc-core/files/oui.lua
@@ -7,7 +7,6 @@ local uci = require "uci"
 local crypt = require "oui.c".crypt
 local statvfs = require "oui.c".statvfs
 local parse_route_addr = require "oui.c".parse_route_addr
-local parse_route6_addr = require "oui.c".parse_route6_addr
 local parse_flow = require "oui.c".parse_flow
 local readdir = require "oui.c".readdir
 
@@ -250,53 +249,6 @@ local methods = {
             end, {data = libubus.STRING}
         }
     },
-    ["oui.opkg"] = {
-        list = {
-            function(req, msg)
-                ubus.reply(req, opkg_list("list", msg))
-            end, {limit = libubus.INT32, offset = libubus.INT32, pattern = libubus.STRING}
-        },
-        list_installed = {
-            function(req, msg)
-                ubus.reply(req, opkg_list("list-installed", msg))
-            end, {limit = libubus.INT32, offset = libubus.INT32, pattern = libubus.STRING}
-        },
-        list_upgradable = {
-            function(req, msg)
-                ubus.reply(req, opkg_list("list-upgradable", msg))
-            end, {limit = libubus.INT32, offset = libubus.INT32}
-        },
-        install = {
-            function(req, msg)
-                if not msg.package or #msg.package == 0 then
-                    return UBUS_STATUS_INVALID_ARGUMENT
-                end
-
-                local r = ubus.call("file", "exec", {command = "opkg", params = {"--force-overwrite", "install", msg.package}})
-                ubus.reply(req, r)
-            end, {package = libubus.STRING}
-        },
-        remove = {
-            function(req, msg)
-                if not msg.package or #msg.package == 0 then
-                    return UBUS_STATUS_INVALID_ARGUMENT
-                end
-
-                local r = ubus.call("file", "exec", {command = "opkg", params = {"--force-removal-of-dependent-packages", "remove", msg.package}})
-                ubus.reply(req, r)
-            end, {package = libubus.STRING}
-        },
-        upgrade = {
-            function(req, msg)
-                if not msg.package or #msg.package == 0 then
-                    return UBUS_STATUS_INVALID_ARGUMENT
-                end
-
-                local r = ubus.call("file", "exec", {command = "opkg", params = {"upgrade", msg.package}})
-                ubus.reply(req, r)
-            end, {package = libubus.STRING}
-        },
-    },
     ["oui.network"] = {
         conntrack_count = {
             function(req, msg)
@@ -389,42 +341,6 @@ local methods = {
                 ubus.reply(req, {leases = leases})
             end, {}
         },
-        dhcp6_leases = {
-            function(req, msg)
-                local leases = {}
-
-                local r, lines = pcall(io.lines, "/tmp/hosts/6relayd")
-                if r then
-                    for line in lines do
-                        local duid, name, ts, addr = line:match("^[^#]%S+ (%S+) +%S+ +(%S+) +(%S+) +%S+ +%S+ +(%S+)")
-                        leases[#leases + 1] = {
-                            ip6addr = addr,
-                            duid = duid,
-                            name = name ~= "-" and name,
-                            expires = ts - os.time()
-                        }
-                    end
-                else
-                    local r, lines = pcall(io.lines, dnsmasq_leasefile())
-                    if r then
-                        for line in lines do
-                            local ts, mac, addr, name, duid  = line:match("(%S+) +(%S+) +(%S+) +(%S+) +(%S+)")
-
-                            if ts and mac and addr and name and duid and addr:find(":") then
-                                leases[#leases + 1] = {
-                                    ip6addr = addr,
-                                    duid = duid ~= "*" and name,
-                                    name = name ~= "*" and name,
-                                    expires = ts - os.time()
-                                }
-                            end
-                        end
-                    end
-                end
-
-                ubus.reply(req, {leases = leases})
-            end, {}
-        },
         routes = {
             function(req, msg)
                 local routes = {}
@@ -446,133 +362,16 @@ local methods = {
                 ubus.reply(req, {routes = routes})
             end, {}
         },
-        routes6 = {
-            function(req, msg)
-                local routes = {}
-                for line in io.lines("/proc/net/ipv6_route") do
-                    local field = {}
-                    for e in line:gmatch("%S+") do
-                        field[#field + 1] = e
-                    end
-
-                    routes[#routes + 1] = {
-                        target = parse_route6_addr(field[1], field[2]),
-                        source = parse_route6_addr(field[3], field[4]),
-                        nexthop = parse_route6_addr(field[5]),
-                        metric = tonumber(field[6], 16),
-                        device = field[10]
-                    }
-                end
-                ubus.reply(req, {routes = routes})
-            end, {}
-        },
-        switch_info = {
-            function(req, msg)
-                if not msg.switch then return UBUS_STATUS_INVALID_ARGUMENT end
-
-                local info = {}
-                local f = io.popen("swconfig dev " .. msg.switch .. " help")
-                if f then
-                    local line = f:read("*l")
-                    local model, num_ports, cpu_port, num_vlans = line:match("%((%S+)%), ports: (%d+) %(cpu @ (%d+)%), vlans: (%d+)")
-
-                    info.model = model
-                    info.num_ports = num_ports
-                    info.cpu_port = cpu_port
-                    info.num_vlans = num_vlans
-
-                    local attributes
-                    for line in f:lines() do
-                        local typ = line:match("%-%-(%S+)")
-                        if typ then
-                            info[typ] = {}
-                            attributes = info[typ]
-                        else
-                            local typ, name, description = line:match("%((%a+)%): (%S+) %((.+)%)$")
-                            if typ then
-                                attributes[#attributes + 1] = {
-                                    type = typ,
-                                    name = name,
-                                    description = description
-                                }
-                            end
-                        end
-                    end
-
-                    f:close()
-                end
-                ubus.reply(req, {info = info})
-            end, {switch = libubus.STRING}
-        },
-        switch_status = {
-            function(req, msg)
-                if not msg.switch then return UBUS_STATUS_INVALID_ARGUMENT end
-
-                local ports = {}
-                local f = io.popen("swconfig dev " .. msg.switch .. " show")
-                if f then
-                    for line in f:lines() do
-                        if line:match("link") then
-                            local up = false
-                            local speed = 0
-                            local duplex = false
-                            local txflow = false
-                            local rxflow = false
-                            local aneg = false
-
-                            for field in line:gmatch("%S+") do
-                                if field:match("link:up") then
-                                    up = true
-                                elseif field:match("speed:") then
-                                    speed = tonumber(field:match("speed:(%d+)"))
-                                elseif field == "txflow" then
-                                    txflow = true
-                                elseif field == "rxflow" then
-                                    rxflow = true
-                                elseif field == "full-duplex" then
-                                    duplex = true
-                                elseif field == "auto" then
-                                    aneg = true
-                                end
-                            end
-
-                            ports[#ports + 1] = {
-                                link = up,
-                                rx_flow_control = rxflow,
-                                tx_flow_control = txflow,
-                                full_duplex = duplex,
-                                auto_negotiation = aneg,
-                                speed = speed
-                            }
-                        end
-                    end
-                    f:close()
-                end
-                ubus.reply(req, {ports = ports})
-            end, {switch = libubus.STRING}
-        },
         ping = {
             function(req, msg)
                 if not msg.name then return UBUS_STATUS_INVALID_ARGUMENT end
                 ubus.reply(req, network_cmd(msg.name, "ping"))
             end, {name = libubus.STRING}
         },
-        ping6 = {
-            function(req, msg)
-                if not msg.name then return UBUS_STATUS_INVALID_ARGUMENT end
-                ubus.reply(req, network_cmd(msg.name, "ping6"))
-            end, {name = libubus.STRING}
-        },
         traceroute = {
             function(req, msg)
                 if not msg.name then return UBUS_STATUS_INVALID_ARGUMENT end
                 ubus.reply(req, network_cmd(msg.name, "traceroute"))
-            end, {name = libubus.STRING}
-        },
-        traceroute6 = {
-            function(req, msg)
-                if not msg.name then return UBUS_STATUS_INVALID_ARGUMENT end
-                ubus.reply(req, network_cmd(msg.name, "traceroute6"))
             end, {name = libubus.STRING}
         },
         nslookup = {
@@ -616,210 +415,6 @@ local methods = {
         }
     },
     ["oui.system"] = {
-        syslog = {
-            function(req, msg)
-                local log = {}
-                local f = io.popen("logread")
-                if f then
-                    for line in f:lines() do
-                        local week, month, day, time, year, facility, level, msg = line:match("(%S+) +(%S+) +(%d+) +(%S+) +(%d+) +(%S+)%.(%S+) +(.+)$")
-                        log[#log + 1] = {
-                            datetime = string.format("%s %s %s %s %s", week, month, day, time, year),
-                            facility = facility,
-                            level = level,
-                            msg = msg
-                        }
-                    end
-                    f:close()
-                end
-                ubus.reply(req, {log = log})
-            end, {}
-        },
-        dmesg = {
-            function(req, msg)
-                local log = {}
-                local f = io.popen("dmesg")
-                if f then
-                    for line in f:lines() do
-                        local time, msg = line:match("(%d+%.%d+)%] +(.+)$")
-                        log[#log + 1] = {
-                            time = time,
-                            msg = msg
-                        }
-                    end
-                    f:close()
-                end
-                ubus.reply(req, {log = log})
-            end, {}
-        },
-        diskfree = {
-            function(req, msg)
-                local fslist = {
-                    root = "/",
-                    tmp = "/tmp"
-                }
-                local resp = {}
-
-                for name, path in pairs(fslist) do
-                    local total, free, used = statvfs(path)
-                    if total then
-                        resp[name] = {
-                            total = total,
-                            free = free,
-                            used = used
-                        }
-                    end
-                end
-
-                ubus.reply(req, resp)
-            end, {}
-        },
-        process_list = {
-            function(req, msg)
-                local processes = {}
-                local f = io.popen("top -bn1")
-                if f then
-                    for line in f:lines() do
-                        local pid, ppid, user, stat, vsz, pvsz, pcpu, cmd = line:match("(%d+) +(%d+) +(%S+) +(%S+) +(%d+) +(%d+)%% +(%d+)%% +(.+)$")
-                        if pid then
-                            processes[#processes + 1] = {
-                                pid = tonumber(pid),
-                                ppid = tonumber(ppid),
-                                user = user,
-                                stat = stat,
-                                vsize = tonumber(vsz),
-                                vsize_percent = tonumber(pvsz),
-                                cpu_percent = tonumber(pcpu),
-                                command = cmd
-                            }
-                        end
-                    end
-                    f:close()
-                end
-                ubus.reply(req, {processes = processes})
-            end, {}
-        },
-        process_signal = {
-            function(req, msg)
-                if not msg.pid or not msg.signal then return UBUS_STATUS_INVALID_ARGUMENT end
-                local cmd = string.format("kill -%d %d", msg.signal, msg.pid)
-                os.execute(cmd)
-            end, {pid = libubus.INT32, signal = libubus.INT32}
-        },
-        init_list = {
-            function(req, msg)
-                local initscripts = {}
-                local f = io.popen("ls /etc/init.d")
-                if f then
-                    for name in f:lines() do
-                        local start, stop, enabled = false
-                        local line = read_file("/etc/init.d/" .. name, "*l")
-                        if line and line:match("/etc/rc.common") then
-                            for line in io.lines("/etc/init.d/" .. name) do
-                                local k, v = line:match("(%S+)=(%d+)")
-                                if k == "START" then
-                                    start = v
-                                elseif k == "STOP" then
-                                    stop = v
-                                    break
-                                end
-                            end
-                            if start then
-                                if read_file("/etc/rc.d/S" .. start .. name, 1) then
-                                    enabled = true
-                                end
-
-                                initscripts[#initscripts + 1] = {
-                                    name = name,
-                                    start = tonumber(start),
-                                    stop = tonumber(stop),
-                                    enabled = enabled
-                                }
-                            end
-                        end
-                    end
-                    f:close()
-                end
-                ubus.reply(req, {initscripts = initscripts})
-            end, {}
-        },
-        init_action = {
-            function(req, msg)
-                if not msg.name then
-                    return UBUS_STATUS_INVALID_ARGUMENT
-                end
-
-                if msg.action ~= "start" and msg.action ~= "stop" and
-                    msg.action ~= "reload" and msg.action ~= "restart" and
-                    msg.action ~= "enable" and msg.action ~= "disable" then
-                    return UBUS_STATUS_INVALID_ARGUMENT
-                end
-
-                local cmd = string.format("/etc/init.d/%s %s &", msg.name, msg.action)
-                os.execute(cmd)
-            end, {name = libubus.STRING, action = libubus.STRING}
-        },
-        crontab_get = {
-            function(req, msg)
-                local entries = {}
-                local r, lines = pcall(io.lines, "/etc/crontabs/root")
-                if r then
-                    for line in lines do
-                        local min, hour, day, month, week, cmd = line:match("(%S+) +(%S+) +(%S+) +(%S+) +(%S+) +(%S+)")
-                        entries[#entries + 1] = {
-                            min = min,
-                            hour = hour,
-                            day = day,
-                            month = month,
-                            week = week,
-                            command = cmd
-                        }
-                    end
-                end
-                ubus.reply(req, {entries = entries})
-            end, {}
-        },
-        crontab_set = {
-            function(req, msg)
-                if not msg.data then return UBUS_STATUS_INVALID_ARGUMENT end
-                os.execute("mkdir -p /etc/crontabs")
-                write_file("/etc/crontabs/root", msg.data)
-            end, {data = libubus.STRING}
-        },
-        led_list = {
-            function(req, msg)
-                local leds = {}
-                local f = io.popen("ls /sys/class/leds")
-                if f then
-                    for name in f:lines() do
-                        local data = read_file("/sys/class/leds/" .. name .. "/trigger")
-                        local active_trigger, brightness, max_brightness
-                        local triggers = {}
-
-                        for trigger in data:gmatch("%S+") do
-                            if trigger:sub(1, 1) == "[" then
-                                trigger = trigger:sub(2, #trigger - 1)
-                                active_trigger = trigger
-                            end
-                            triggers[#triggers + 1] = trigger
-                        end
-
-                        brightness = read_file("/sys/class/leds/" .. name .. "/brightness", "*l")
-                        max_brightness = read_file("/sys/class/leds/" .. name .. "/max_brightness", "*l")
-
-                        leds[#leds + 1] = {
-                            name = name,
-                            triggers = triggers,
-                            active_trigger = active_trigger,
-                            brightness = tonumber(brightness),
-                            max_brightness = tonumber(max_brightness)
-                        }
-                    end
-                    f:close()
-                end
-                ubus.reply(req, {leds = leds})
-            end, {}
-        },
         backup_restore = {
             function(req, msg)
                 local resp = ubus.call("file", "exec", {command = "sysupgrade", params = {"--restore-backup", '/tmp/backup.tar.gz'}})
@@ -840,33 +435,6 @@ local methods = {
                 end
                 ubus.reply(req, {times = times})
             end, {}
-        },
-        read_dir = {
-            function(req, msg)
-                local path = msg.path or "/"
-                local files = readdir(path)
-
-                path = path:gsub("/$", "")
-
-                for _, file in ipairs(files) do
-                    file.path = path .. "/" .. file.name
-                end
-
-                ubus.reply(req, {files = files})
-            end, {path = libubus.STRING}
-        },
-        new_file = {
-            function(req, msg)
-                local path = msg.path
-                local dir = msg.dir
-                local cmd = "touch"
-
-                if dir then cmd = "mkdir" end
-
-                local r = os.execute(cmd .. " " .. path)
-
-                ubus.reply(req, {r = r})
-            end, {path = libubus.STRING, dir = libubus.BOOLEAN}
         }
     }
 }
