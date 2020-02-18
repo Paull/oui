5a0c24ce389f6651420b4f5b5a7f54c4070888be
Paull <paull@qq.com>
2020-02-18 15:14:13 +0800

    remove unnecessary dnat settings

--- oui-ui-core/src/src/views/network/dnat.vue
+++ oui-ui-core/src/src/views/network/dnat.vue
@@ -4,18 +4,9 @@
       <uci-option-input :label="$t('Name')" name="name"></uci-option-input>
       <uci-option-list :label="$t('Protocol')" name="proto" :options="protos" initial="tcp udp" allow-create></uci-option-list>
       <uci-option-input :label="$t('External port')" name="src_dport" rules="port"></uci-option-input>
-      <uci-option-list :label="$t('Internal zone')" name="dest" :options="zones" required></uci-option-list>
       <uci-option-list :label="$t('Internal IP address')" name="dest_ip" :options="arps" allow-create rules="ip4addr"></uci-option-list>
       <uci-option-input :label="$t('Internal port')" name="dest_port" rules="port"></uci-option-input>
       <uci-option-switch :label="$t('Enable')" name="enabled" initial="1"></uci-option-switch>
-      <uci-table-expand>
-        <uci-option-list :label="$t('Source zone')" name="src" value="wan" :options="zones" required></uci-option-list>
-        <uci-option-dlist :label="$t('Source MAC address')" name="src_mac" rules="macaddr"></uci-option-dlist>
-        <uci-option-input :label="$t('Source IP address')" name="src_ip" rules="ip4addr" :placeholder="$t('any')"></uci-option-input>
-        <uci-option-input :label="$t('Source port')" name="src_port" rules="port" :placeholder="$t('any')" depend="proto == 'tcp' || proto == 'udp' || proto == 'tcp udp'"></uci-option-input>
-        <uci-option-input :label="$t('External IP address')" name="src_dip" rules="ip4addr" :placeholder="$t('any')"></uci-option-input>
-        <uci-option-switch :label="$t('Enable NAT Loopback')" name="reflection" initial="1"></uci-option-switch>
-      </uci-table-expand>
     </uci-section>
   </uci-form>
 </template>
@@ -30,20 +21,20 @@ export default {
         ['tcp udp', 'TCP+UDP'],
         ['icmp', 'ICMP']
       ],
-      zones: [],
       arps: []
     }
   },
   methods: {
     load() {
       this.$firewall.load(true);
-      this.zones = this.$firewall.zones.map(z => z.name());
     },
     filterDnat(s) {
       return s.target !== 'SNAT';
     },
     afterAdd(sid) {
       this.$uci.set('firewall', sid, 'target', 'DNAT');
+      this.$uci.set('firewall', sid, 'dest', 'lan');
+      this.$uci.set('firewall', sid, 'src', 'wan');
     }
   },
   created() {
